11ad583d0453dc134cc0f0783a7fbc30553d27b9
forbidden Betta <forbiddenBetta@gmail.com>
2022-04-06 16:10:00 +0200

    Work

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,10 +2,12 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Professor;
 use App\Entity\Student;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
 
 class AppFixtures extends Fixture
 {
@@ -21,23 +23,31 @@ class AppFixtures extends Fixture
         // var_dump($myarray);
         // var_dump($myarray["CP"]);
 
+        var_dump("Student: ");
         
         foreach ($myarray["CP"] as $key => $value) {
-            // var_dump("Student: ");
             // var_dump($myarray["CP"][$key0]);
             $pieces = explode(" ", $myarray["CP"][$key]);
             $student = new Student();
             $student->setFirstName($pieces[0]);
             $student->setName($pieces[1]);
-            $student->setEmail(($faker->email));
             $student->setUsername($student->getName() . $student->getFirstName() . "du" . rand(0, 100));
-            $student->setParentEmail1(($student->getName() . $faker->firstName . "@" . $faker->freeEmailDomain));
-            $student->setParentEmail2(($faker->email));
+            $student->setEmail(($student->getUsername() . rand() . "@" . $faker->freeEmailDomain));
+            $student->setParentEmail1(($student->getName() . $faker->firstName . $faker->cityPrefix . "@" . $faker->freeEmailDomain));
+            $student->setParentEmail2(($student->getName() . $faker->firstName . $faker->cityPrefix . "@" . $faker->freeEmailDomain));
             $student->setGender((bool) mt_rand(0, 1));
             $student->setPassword(rand());
 
             $manager->persist($student);
             $manager->flush();
         }
+
+        Var_dump("Professor: ");
+        $prof = new Professor();
+        $prof->setAge(rand(18, 60));
+        $prof->setArrivalDate(new \DateTime());
+        $prof->setSalary(rand(1000, 2300));
+        $prof->setSection($manager->getRepository(Section::class)->find(1));
+
     }
 }
--- src/Entity/Professor.php
+++ src/Entity/Professor.php
@@ -6,6 +6,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ProfessorRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+
 #[ORM\Entity(repositoryClass: ProfessorRepository::class)]
 #[ApiResource(normalizationContext: ['groups' => ['read_professor']], denormalizationContext: ['groups' => ['write_professor']])]
 
@@ -14,23 +15,23 @@ class Professor extends Person
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
-    #[Groups(['read_professor' ])]
+    #[Groups(['read_professor'])]
     private $id;
 
     #[ORM\Column(type: 'integer')]
-    #[Groups(['read_professor', 'write_professor' ])]
+    #[Groups(['read_professor', 'write_professor'])]
     private $Age;
 
     #[ORM\Column(type: 'date')]
-    #[Groups(['read_professor', 'write_professor' ])]
+    #[Groups(['read_professor', 'write_professor'])]
     private $ArrivalDate;
 
     #[ORM\Column(type: 'integer')]
-    #[Groups(['read_professor', 'write_professor' ])]
+    #[Groups(['read_professor', 'write_professor'])]
     private $Salary;
 
     #[ORM\OneToOne(mappedBy: 'Instit', targetEntity: Section::class, cascade: ['persist', 'remove'])]
-    #[Groups(['read_professor', 'write_professor' ])]
+    #[Groups(['read_professor', 'write_professor'])]
     private $section;
 
     public function getId(): ?int
