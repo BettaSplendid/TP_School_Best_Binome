961ed0e7aef2e6aa00acdc91bfd28985b6c77a10
forbidden Betta <forbiddenBetta@gmail.com>
2022-04-06 16:41:12 +0200

    Push stuff

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,11 +3,11 @@
 namespace App\DataFixtures;
 
 use App\Entity\Professor;
+use App\Entity\Section;
 use App\Entity\Student;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
-use phpDocumentor\Reflection\DocBlock\Tags\Var_;
 
 class AppFixtures extends Fixture
 {
@@ -24,7 +24,7 @@ class AppFixtures extends Fixture
         // var_dump($myarray["CP"]);
 
         var_dump("Student: ");
-        
+
         foreach ($myarray["CP"] as $key => $value) {
             // var_dump($myarray["CP"][$key0]);
             $pieces = explode(" ", $myarray["CP"][$key]);
@@ -42,12 +42,55 @@ class AppFixtures extends Fixture
             $manager->flush();
         }
 
+        var_dump("Section: ");
+        $cp = new Section();
+        $cp->setName("CP");
+        $manager->persist($cp);
+
+        $ce1 = new Section();
+        $ce1->setName("CE1");
+        $manager->persist($ce1);
+
+
+        $ce2 = new Section();
+        $ce2->setName("CE2");
+        $manager->persist($ce2);
+
+
+        $cm1 = new Section();
+        $cm1->setName("CM1");
+        $manager->persist($cm1);
+
+
+        $cm2 = new Section();
+        $cm2->setName("CM2");
+        $manager->persist($cm2);
+        $manager->flush();
+
         Var_dump("Professor: ");
         $prof = new Professor();
+        $prof->setName("Delenoix");
+        $prof->setFirstName("Jean");
+        $prof->SetEmail($prof->getName() . $prof->getFirstName() . "@" . $faker->freeEmailDomain);
+        $prof->setUsername($prof->getName() . $prof->getFirstName() . "teach");
+        $prof->setPassword(rand());
+
         $prof->setAge(rand(18, 60));
         $prof->setArrivalDate(new \DateTime());
         $prof->setSalary(rand(1000, 2300));
-        $prof->setSection($manager->getRepository(Section::class)->find(1));
+        $prof->setSection($manager->getRepository(Section::class)->findBy(['Name' => 'CP'])[0]);
+        $manager->persist($prof);
+        $manager->flush();
+
+
+
+        // private $email;
+        // private $roles = [];
+        // private $password;
+        // private $Username;
+        // private $Name;
+        // private $FirstName;
+
 
     }
 }
