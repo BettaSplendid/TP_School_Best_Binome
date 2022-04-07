1a95a379a2293f29a92d5410f9ec2633233d433b
Aelie54 <klein.amelie1991@gmail.com>
2022-04-07 15:53:18 +0200

    username pour connection et groups dans person

--- src/Entity/Person.php
+++ src/Entity/Person.php
@@ -6,6 +6,8 @@ use App\Repository\PersonRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\JoinColumn;
 use Doctrine\ORM\Mapping\OneToOne;
@@ -19,30 +21,38 @@ use Doctrine\ORM\Mapping\DiscriminatorColumn;
 #[ORM\MappedSuperclass(PersonRepository::class)]
 #[ORM\InheritanceType("JOINED")]
 #[ORM\DiscriminatorColumn('role_type', "string")]
+#[ApiResource(normalizationContext: ['groups' => ['read_person']], denormalizationContext: ['groups' => ['write_person']])]
 abstract class Person implements UserInterface, PasswordAuthenticatedUserInterface
 {
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['read_person'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 180, unique: true)]
+    #[Groups(['read_person', 'write_person' ])]
     private $email;
 
     #[ORM\Column(type: 'json')]
+    #[Groups(['read_person', 'write_person' ])]
     private $roles = [];
 
     #[ORM\Column(type: 'string')]
+    #[Groups(['read_person', 'write_person' ])]
     private $password;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    #[Groups(['read_person', 'write_person' ])]
     private $Username;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read_person', 'write_person' ])]
     private $Name;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read_person', 'write_person' ])]
     private $FirstName;
 
     public function getId(): ?int
